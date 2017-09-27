baf294a6b5028281d907ee028aa752af1c1ad718
Rico Evers <ricoevers@live.nl>
2017-09-27 23:20:46 +0200

    First Commit

---
+++ Assets/Scripts/DungeonGenerator.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+public class Room
+{
+	public string name;
+	public bool active;
+	public float sizeX;
+	public float sizeY;
+	public Vector2 position;
+
+	public Room(string name, float x, float y, Vector2 position)
+	{
+		this.name = name;
+		sizeX = x;
+		sizeY = y;
+		this.position = position;
+	}
+}
+
+public class DungeonGenerator : MonoBehaviour
+{
+	[SerializeField] private GameObject[] roomObjects;
+	[SerializeField] private int roomCount;
+	private ExtensionNode[] extensionNodes;
+	private Room[] rooms;
+	
+	private void Start()
+	{
+		GenerateRooms();
+		CreateMap();
+	}
+
+	private void GenerateRooms()
+	{
+		rooms = new Room[roomObjects.Length];
+		for (var i = 0; i < roomObjects.Length; i++)
+		{
+			rooms[i] = new Room(roomObjects[i].name, roomObjects[i].transform.localScale.x, roomObjects[i].transform.localScale.z, new Vector2());
+		}
+	}
+
+	private void CreateMap()
+	{
+		while (true)
+		{
+			extensionNodes = FindObjectsOfType<ExtensionNode>();
+
+			if (extensionNodes.Length >= roomCount) return;
+
+			var currentnumber = Random.Range(0, rooms.Length);
+			if (!extensionNodes[currentnumber].CanExtend(rooms[currentnumber].sizeY)) continue;
+			//if (IsColliding) continue;
+			Destroy(extensionNodes[currentnumber].gameObject);
+			Instantiate(roomObjects[Random.Range(0, roomObjects.Length)], extensionNodes[currentnumber].transform.position, extensionNodes[currentnumber].transform.rotation);
+		}
+	}
+}
---
+++ Assets/Scripts/ExtensionNode.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class ExtensionNode : MonoBehaviour
+{
+	private float maxDistance = 20;
+
+	private void OnDrawGizmos()
+	{
+		Debug.DrawRay(transform.position, transform.forward);
+	}
+
+	public bool CanExtend(float sizeObject)
+	{
+		RaycastHit hit;
+		if (!Physics.Raycast(transform.position, transform.forward, out hit, maxDistance)) return true;
+		if (Physics.CheckSphere(transform.position + transform.forward, 10)) return false;
+		return !(hit.distance < sizeObject);
+	}
+}
